7dabf27d2e84a4c34bff3fa6bd6d5dc38b083b78
Horizonte Jr <horizonte.junior@edu.sc.senai.br>
2026-05-05 20:51:22 -0300

    retirado componentes

--- src/Components/Carrinho.js
+++
@@ -1,9 +0,0 @@
-import { useState} from "react";
-
-export default function Carrinho(){
-
-
-const carrinhoSalvo = LocalStorage.getIten("carrinho");
-return carrinhoSalvo ? JSON.parse(carrinhoSalvo) : [];
-
-}
